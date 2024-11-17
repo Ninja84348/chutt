7f5fac50c2ba1db667cb1ce764fc1e966499a621
Ninja84348 <nn4766189@gmail.com>
2024-11-17 10:49:08 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7850149668:AAHBI1W2q4iYgjufxwRtW-n1D1fgwsPcaQI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RIYA6E")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5829441229")
+
+
+
+
+
+  
